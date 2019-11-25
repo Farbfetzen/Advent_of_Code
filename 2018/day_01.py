3ddad639a4ff4af94c877b2bbe88addb4c33b18f
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-11-25 23:37:19 +0100

    Initial commit

---
+++ 2018/day_01.py
@@ -0,0 +1,27 @@
+import os
+
+
+with open("day_01_input.txt", "r") as file:
+    changes = [int(i) for i in file.read().splitlines()]
+
+
+# part 1
+print(sum(changes))
+
+
+# part 2
+frequency = 0
+i = 0
+seen = set()
+
+while True:
+    for change in changes:
+        if frequency in seen:
+            break
+        seen.add(frequency)
+        frequency += change
+    else:
+        continue
+    break
+
+print(frequency)
---
+++ 2018/day_02.py
@@ -0,0 +1,35 @@
+import collections
+
+
+with open("day_02_input.txt", "r") as file:
+    IDs = file.read().splitlines()
+
+
+# part 1
+sum_2 = 0
+sum_3 = 0
+for ID in IDs:
+    count = collections.Counter(ID)
+    sum_2 += 2 in count.values()
+    sum_3 += 3 in count.values()
+print(sum_2 * sum_3)
+
+
+# part 2
+for i, ID in enumerate(IDs):
+    for other_ID in IDs:
+        count_differences = 0
+        for a, b in zip(ID, other_ID):
+            count_differences += a != b
+        if count_differences == 1:
+            break
+    else:
+        continue
+    break
+
+for x in enumerate(zip(ID, other_ID)):
+    i = x[0]
+    a, b = x[1]
+    if a != b:
+        break
+print(ID[:i] + ID[i+1:])
