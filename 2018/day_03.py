983563b28569857efc5f919256f977913e6b6a31
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-11-26 13:55:15 +0100

    Fix day_03 part 2

--- 2018/day_03.py
+++ 2018/day_03.py
@@ -13,6 +13,7 @@ with open("day_03_input.txt", "r") as file:
 
 
 all_coordinates = {}
+all_coordinates_list = []
 for claim in claims:
     ID, left, top, width, height = claim
     claim_xy = []
@@ -20,10 +21,7 @@ for claim in claims:
         for y in range(top+1, top+height+1):
             claim_xy.append(str(x)+" "+str(y))
     all_coordinates[ID] = claim_xy
-
-all_coordinates_list = []
-for v in all_coordinates.values():
-    all_coordinates_list += list(v)
+    all_coordinates_list += claim_xy
 
 counter_1 = collections.Counter(all_coordinates_list)
 counter_2 = collections.Counter(counter_1.values())
@@ -31,32 +29,8 @@ if (1 in counter_2):
     del counter_2[1]
 print(sum(counter_2.values()))  # part 1
 
-
-# alternativer Ansatz für part 2:
-# 1. Suche dir aus dem counter_1 alle xy aus, die nur einmal vorkommen.
-# 2. Suche den claim, der nur aus diesen coordinaten besteht.
-
-
-
-# This is horrible nested mess but I'm too tired to fix it right now.
-#def foo(coords):
-    #IDs = list(coords.keys())
-    #while True:
-        #ID = IDs.pop()
-        #claim = coords.pop(ID)
-        #found = False
-        #for k, v in coords.items():
-            #for xy in claim:
-                #if xy in v:
-                    #found = True
-                    #del coords[k]
-                    #IDs.remove(k)
-                    #break
-            #if found:
-                #break
-        #if not found:
-            #return ID
-#print(foo(all_coordinates))
-
-
-# 1244 ist falsch (zu hoch)
+unique = {k for k, v in counter_1.items() if v == 1}
+for ID, claim in all_coordinates.items():
+    if set(claim).issubset(unique):
+        break
+print(ID)  # part 2
