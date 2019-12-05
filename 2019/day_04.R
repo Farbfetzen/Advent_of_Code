65923222d5ec9eb5d7e0577c3487f8c082c8c75b
Sebastian Henz <41594266+BastiHz@users.noreply.github.com>
2019-12-05 11:13:03 +0100

    Change to a solution using *apply functions

--- 2019/day_04.R
+++ 2019/day_04.R
@@ -1,17 +1,10 @@
 # https://adventofcode.com/2019/day/4
 
 
-# At first I tried to do it in a vectorized way but that was far slower than
-# this simple loop.
-
 passwords <- strsplit(as.character(178416:676461), "")
-total_1 <- 0
-total_2 <- 0
-for (pw in passwords) {
-    if (is.unsorted(pw)) next
-    freq <- table(pw)
-    total_1 <- total_1 + any(freq > 1)
-    total_2 <- total_2 + (2 %in% freq)
-}
+sorted <- passwords[!vapply(passwords, is.unsorted, TRUE)]
+freq <- lapply(sorted, table)
+total_1 <- sum(vapply(freq, function(x) any(x > 1), TRUE))
+total_2 <- sum(vapply(freq, function(x) 2 %in% x, TRUE))
 print(total_1)  # 1650
 print(total_2)  # 1129
