de463cf10864bfccc89a5b50e2f7fa55f0416aed
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-12-07 18:29:10 +0100

    Day 5 part 2 done

--- 2019/day_05.py
+++ 2019/day_05.py
@@ -13,7 +13,11 @@ class IntcodeComputer:
             1: self.add,
             2: self.mult,
             3: self.read,
-            4: self.print
+            4: self.print,
+            5: self.jump_if_true,
+            6: self.jump_if_false,
+            7: self.less_than,
+            8: self.equals
         }
 
     def run(self):
@@ -53,12 +57,46 @@ class IntcodeComputer:
 
     def read(self):
         target_position = self.intcode[self.pointer + 1]
-        self.intcode[target_position] = int(input("enter an integer: "))
+        while True:
+            try:
+                value = int(input("Enter an integer: "))
+                break
+            except ValueError:
+                print("Not an integer. Try again.")
+        self.intcode[target_position] = value
         self.pointer += 2
 
     def print(self):
         print(self.get_values(1)[0])
         self.pointer += 2
+
+    def jump_if_true(self):
+        values = self.get_values(2)
+        if values[0] != 0:
+            self.pointer = values[1]
+        else:
+            self.pointer += 3
+
+    def jump_if_false(self):
+        values = self.get_values(2)
+        if values[0] == 0:
+            self.pointer = values[1]
+        else:
+            self.pointer += 3
+
+    def less_than(self):
+        values = self.get_values(2)
+        output = int(values[0] < values[1])
+        target_position = self.intcode[self.pointer + 3]
+        self.intcode[target_position] = output
+        self.pointer += 4
+
+    def equals(self):
+        values = self.get_values(2)
+        output = int(values[0] == values[1])
+        target_position = self.intcode[self.pointer + 3]
+        self.intcode[target_position] = output
+        self.pointer += 4
 
 
 with open("day_05_input.txt", "r") as file:
@@ -66,4 +104,5 @@ with open("day_05_input.txt", "r") as file:
 computer = IntcodeComputer(program)
 computer.run()
 
-# Solution of part 1: 9006673
+# Part 1: input = 1, output = a bunch of zeros and then 9006673
+# Part 2: input = 5, output = 3629692
