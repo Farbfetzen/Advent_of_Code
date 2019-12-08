b838770ae7f4cc66fd8d2f04bbcd060b77cc7e3b
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-12-08 14:01:06 +0100

    Day 8 done

---
+++ 2019/day_08.py
@@ -0,0 +1,48 @@
+# https://adventofcode.com/2019/day/8
+
+
+import math
+
+with open("day_08_input.txt") as file:
+    transmission = [int(i) for i in file.read().strip()]
+width = 25
+height = 6
+pixels_per_layer = width * height
+assert len(transmission) % pixels_per_layer == 0
+layers = []
+while transmission:
+    layers.append(transmission[:pixels_per_layer])
+    del transmission[:pixels_per_layer]
+
+# part 1:
+n_zero = math.inf
+n_zero_current = 0
+layer_with_fewest_0 = []
+for layer in layers:
+    n_zero_current = len([i for i in layer if i == 0])
+    if n_zero_current < n_zero:
+        n_zero = n_zero_current
+        layer_with_fewest_0 = layer
+n_1 = len([i for i in layer_with_fewest_0 if i == 1])
+n_2 = len([i for i in layer_with_fewest_0 if i == 2])
+print(n_1 * n_2)  # 2520
+
+# part 2:
+visible = []
+for i in range(pixels_per_layer):
+    for layer in layers:
+        color = layer[i]
+        if color < 2:
+            visible.append(color)
+            break
+image = ""
+x = 0
+for y in range(height):
+    for pixel in (i for i in visible[x:x+width]):
+        if pixel == 0:
+            image += " "
+        elif pixel == 1:
+            image += "#"
+    image += "\n"
+    x += width
+print(image)  # LEGJY
