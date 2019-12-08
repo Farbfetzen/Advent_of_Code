1aa82d75e457adc3fa30697d5d2a8e45f5f78896
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-12-08 19:01:26 +0100

    Plot the image with matplotlib

--- 2019/day_08.py
+++ 2019/day_08.py
@@ -2,6 +2,7 @@
 
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 with open("day_08_input.txt") as file:
@@ -18,11 +19,16 @@ layers = np.array(transmission).reshape(n_layers, pixels_per_layer)
 fewest_0_layer = layers[np.count_nonzero(layers, 1).argmax(), :]
 print((fewest_0_layer == 1).sum() * (fewest_0_layer == 2).sum())  # 2520
 
-# part 2:
-visible = np.apply_along_axis(lambda x: x[np.argmax(x < 2)], 0, layers)
-visible = visible.reshape(height, width).astype(str)
-visible[visible == "0"] = " "
-visible[visible == "1"] = "#"
-visible = np.apply_along_axis(lambda x: "".join(i for i in x), 1, visible)
-visible = "\n".join(visible)
-print(visible)  # LEGJY
+# part 2 as a console printout and an image:
+image = np.apply_along_axis(lambda x: x[np.argmax(x < 2)], 0, layers)
+image = image.reshape(height, width)
+plt.imshow(image, "binary")
+plt.show()  # LEGJY
+
+# alternatively as a console printout:
+# image = image.astype(str)
+# image[image == "0"] = " "
+# image[image == "1"] = "#"
+# image = np.apply_along_axis(lambda x: "".join(i for i in x), 1, image)
+# image = "\n".join(image)
+# print(image)  # LEGJY
