88eb01bf016b874f6ca09e04701404a1502933b9
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-12-08 21:34:46 +0100

    Fun with compact pipelines in day 4

--- 2019/day_04.R
+++ 2019/day_04.R
@@ -11,3 +11,29 @@ total_1 <- sum(vapply(freq, function(x) any(x > 1), TRUE))
 total_2 <- sum(vapply(freq, function(x) 2 %in% x, TRUE))
 print(total_1)  # 1650
 print(total_2)  # 1129
+
+
+# And just for fun here is the solution as a single magrittr pipeline:
+library(magrittr)
+readLines("2019/day_04_input.txt") %>%
+    strsplit("-") %>%
+    unlist() %>%
+    as.integer() %>%
+    {seq(.[1], .[2])} %>%
+    as.character() %>%
+    strsplit("") %>%
+    .[!vapply(., is.unsorted, TRUE)] %>%
+    lapply(., table) %>%
+    {c(
+        sum(vapply(., function(x) any(x > 1), TRUE)),
+        sum(vapply(., function(x) 2 %in% x, TRUE))
+    )} %>%
+    print()
+
+# And again in a ridiculously compact format:
+as.integer(unlist(strsplit(readLines("2019/day_04_input.txt"), "-"))) %>%
+    {strsplit(as.character(seq(.[1], .[2])), "")} %>%
+    {lapply(.[!vapply(., is.unsorted, TRUE)], table)} %>%
+    {apply(sapply(., function(x) c(any(x > 1), 2 %in% x)), 1, sum)} %>%
+    print()
+
