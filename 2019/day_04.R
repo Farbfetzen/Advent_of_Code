ccfa4545bbef46b9e5d45cb5246d0f4d3cbaa7db
Sebastian Henz <41594266+BastiHz@users.noreply.github.com>
2019-12-13 00:03:38 +0100

    Replace `=` with `<-`

--- 2019/day_04.R
+++ 2019/day_04.R
@@ -1,9 +1,9 @@
 # https://adventofcode.com/2019/day/4
 
 
-password_range = readLines("2019/day_04_input.txt")
-password_range = as.integer(strsplit(password_range, "-")[[1]])
-passwords = password_range[1]:password_range[2]
+password_range <- readLines("2019/day_04_input.txt")
+password_range <- as.integer(strsplit(password_range, "-")[[1]])
+passwords <- password_range[1]:password_range[2]
 passwords <- strsplit(as.character(passwords), "")
 sorted <- passwords[!vapply(passwords, is.unsorted, TRUE)]
 freq <- lapply(sorted, table)
