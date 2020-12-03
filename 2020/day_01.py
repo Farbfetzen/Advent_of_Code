259b9c7e045aab6c97bcb4722c90e1259b254994
BastiHz <bastihz.dev@posteo.de>
2020-12-03 11:14:44 +0100

    2020 day 1 done

---
+++ 2020/day_01.py
@@ -0,0 +1,25 @@
+def part_1(data):
+    for i, x in enumerate(data):
+        y = 2020 - x
+        if y in data[(i+1):]:
+            return x * y
+
+
+def part_2(data):
+    for i, x in enumerate(data):
+        for j, y in enumerate(data[(i+1):]):
+            z = 2020 - x - y
+            if z in data[(i+j):]:
+                return x * y * z
+
+
+test_data = (1721, 979, 366, 299, 675, 1456)
+assert part_1(test_data) == 514579
+assert part_2(test_data) == 241861950
+
+
+with open("day_01_input.txt", "r") as file:
+    expenses = [int(i) for i in file.read().splitlines()]
+
+print(part_1(expenses))  # 436404
+print(part_2(expenses))  # 274879808
