9acd98310808c8c46519c24e3f542b78626042c5
bastihz <bastihz.dev@posteo.de>
2020-12-04 14:23:36 +0100

    2020 day 4 cleaned up a bit

--- 2020/day_04.py
+++ 2020/day_04.py
@@ -17,34 +17,42 @@ def part_2(passports):
     for passport in passports:
         if not all(req in passport for req in REQUIRED):
             continue
+
         if not (1920 <= int(passport["byr"]) <= 2002):
             continue
+
         if not (2010 <= int(passport["iyr"]) <= 2020):
             continue
+
         if not (2020 <= int(passport["eyr"]) <= 2030):
             continue
+
         hgt = passport["hgt"]
         unit = hgt[-2:]
-        if unit not in ("cm", "in"):
-            continue
-        value = int(hgt[:-2])
         if unit == "cm":
-            if not (150 <= value <= 193):
+            if not (150 <= int(hgt[:-2]) <= 193):
                 continue
-        elif not (59 <= value <= 76):
+        elif unit == "in":
+            if not (59 <= int(hgt[:-2]) <= 76):
+                continue
+        else:
             continue
+
         hcl = passport["hcl"]
         valid_hcl = "0123456789abcdef"
         if (hcl[0] != "#"
                 or len(hcl) != 7
                 or any(x not in valid_hcl for x in hcl[1:])):
             continue
+
         valid_ecl = ("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
         if not passport["ecl"] in valid_ecl:
             continue
+
         pid = passport["pid"]
         if len(pid) != 9 or any(n not in "0123456789" for n in pid):
             continue
+
         n_valid += 1
     return n_valid
 
@@ -58,6 +66,5 @@ with open("day_04_input.txt", "r") as file:
             passport[k] = v
         passports.append(passport)
 
-print(part_1(passports))
-print(part_2(passports))
-
+print(part_1(passports))  # 208
+print(part_2(passports))  # 167
