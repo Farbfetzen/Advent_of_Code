eccb59dc637655edc59ea41ad554b9728dfdc18f
bastihz <bastihz.dev@posteo.de>
2020-12-05 22:14:50 +0100

    Test the intcode computer by catching the print output

--- 2019/intcode_test.py
+++ 2019/intcode_test.py
@@ -1,17 +1,11 @@
 """Check if the IntcodeComputer still works correctly after changing it.
+Works by catching all the print outputs into a string and comparing it with the
+expected outputs. This is easier than reworking everything to use a proper
+unittest library.
 """
 
 
-import day_02
-import day_05
-import day_07
-import day_09
-import day_11
-import day_13
-
-
-"""
-3101844
+EXPECTED = """3101844
 8478
 0
 0
@@ -29,12 +23,42 @@ import day_13
 2457252183
 70634
 2428
- ###    ## #    #### ###  #  #  ##  #  #   
- #  #    # #    #    #  # #  # #  # #  #   
- #  #    # #    ###  ###  #  # #    #  #   
- ###     # #    #    #  # #  # #    #  #   
- # #  #  # #    #    #  # #  # #  # #  #   
- #  #  ##  #### #    ###   ##   ##   ##    
+ ███    ██ █    ████ ███  █  █  ██  █  █   
+ █  █    █ █    █    █  █ █  █ █  █ █  █   
+ █  █    █ █    ███  ███  █  █ █    █  █   
+ ███     █ █    █    █  █ █  █ █    █  █   
+ █ █  █  █ █    █    █  █ █  █ █  █ █  █   
+ █  █  ██  ████ █    ███   ██   ██   ██    
 268
 13989
-"""
+""".splitlines()
+
+
+# Source: https://stackoverflow.com/a/40984270
+import io
+from contextlib import redirect_stdout
+
+f = io.StringIO()
+with redirect_stdout(f):
+    import day_02
+    import day_05
+    import day_07
+    import day_09
+    import day_11
+    import day_13
+out = f.getvalue().splitlines()
+
+
+ok = True
+for i, line in enumerate(out):
+    exp = EXPECTED[i]
+    try:
+        assert exp == line
+    except AssertionError:
+        print(f"{line=}")
+        print(f"{exp=}\n")
+        ok = False
+if ok:
+    print("Ok")
+else:
+    print("Found some errors.")
