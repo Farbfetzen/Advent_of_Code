0eecd6d00fd3625582e0aab287f4b4d37e5373cd
bastihz <bastihz.dev@posteo.de>
2020-12-08 23:54:08 +0100

    2019 day 18

--- 2019/day_18.py
+++ 2019/day_18.py
@@ -1,128 +1,141 @@
 # https://adventofcode.com/2019/day/18
 
 
-# This works fine for test inputs 1, 2, 3, and 5 but takes forever on 4.
-# I'm not sure if it ever finishes on 4 and I haven't tested it on the real
-# input yet. Need to find a better solution that runs in a resonable time for
-# many keys.
-
-# Maybe an idea from the solutions thread on Reddit:
-# https://www.reddit.com/r/adventofcode/comments/ec8090/2019_day_18_solutions
-# First get the distances from all keys (and start position) to all other keys
-# and save which doors are in the way:
-# {
-#     "@": {
-#         "a": (2, {}),    # 2 steps to "a", no door blocking
-#         "b": (4, {"A"})  # 4 steps to "b", blocked by door A
-#     },
-#     "a": {"b": (6, {"A"})},
-#     "b": {"a": (6, {"A"})}
-# }
-# Use this as the map.
-# Make sure to not calculate the distance from the keys to the start position!
-# Then recursively go from key to key while keeing track of which doors
-# have already been opened and which keys have been visited.
-# This way I don't have to recalculate all the distances on every step.
-#
-# Also a nice solution I found: https://repl.it/@joningram/AOC-2019#day18.py
-# This one has a nicer BFS and no recursion!
-#
-# Important observation: When searching one level at a time, the first complete
-# path found will be the shortest. So as soon as all keys are found I can
-# break out of the loop. That's also why it might be a good idea to not only
-# remember the blocking doors between keys but also the keys between keys. So
-# I can add them to the collection faster.
+# This was a very hard one for me. I finally gave up and looked at the other
+# solutions in the solutions thread on Reddit and shamelessly adapted my script
+# from this one: https://repl.it/@joningram/AOC-2019#day18.py.
+# I use lots of continue to flatten the nested loops with many ifs.
 
 
-from pprint import pprint
-import math
+import string
 
 
-# Global dictionaries, useful for the recursion.
-maze = {}
-key_positions = {}
-doors_by_position = {}  # position: character
-doors_by_character = {}  # character: position
+KEYS = string.ascii_lowercase
 
 
-def build_maze(maze_input):
-    maze.clear()
-    key_positions.clear()
-    doors_by_position.clear()
-    doors_by_character.clear()
-    start_position = None
-    for y, row in enumerate(maze_input):
+def find_paths(start_x, start_y, maze):
+    """Find the paths from the start to all reachable keys or doors."""
+    seen = {(start_x, start_y)}
+    # Each item in the queue has x, y, distance so far,
+    # and path (keys and doors encountered en route).
+    queue = [(start_x, start_y, 0, "")]
+    paths = {}
+    ignored_symbols = {char for char in ".#@1234"}
+    dxy = ((1, 0), (-1, 0), (0, 1), (0, -1))
+    for (x, y, distance, path) in queue:
+        seen.add((x, y))
+        symbol = maze[y][x]
+        if distance > 0 and symbol not in ignored_symbols:
+            # Found a key or a door.
+            # But keep only the keys as dictionary keys because the
+            # distances between doors are not relevant.
+            if symbol.islower():
+                paths[symbol] = (distance, path)
+            path += symbol
+        # Search for walkable spaces among neigboring positions.
+        distance += 1
+        for (dx, dy) in dxy:
+            nx = x + dx
+            ny = y + dy
+            if (nx, ny) not in seen and maze[ny][nx] != "#":
+                queue.append((nx, ny, distance, path))
+    return paths
+
+
+def build_maze(maze):
+    """Find all points of interest on the map and construct all
+    paths between them.
+    """
+    interesting = {k for k in KEYS}
+    interesting.update("@1234")
+    all_paths = {}
+    for y, row in enumerate(maze):
         for x, symbol in enumerate(row):
-            position = (x, y)
-            if symbol != "#":
-                maze[position] = []
-            if symbol == "@":
-                start_position = position
-            elif symbol.islower():
-                key_positions[position] = symbol
-            elif symbol.isupper():
-                doors_by_position[position] = symbol
-    doors_by_character.update({v: k for k, v in doors_by_position.items()})
-    for pos in maze:
-        for delta_pos in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            neighbor = (pos[0] + delta_pos[0], pos[1] + delta_pos[1])
-            if neighbor in maze:
-                maze[pos].append(neighbor)
-    return start_position
+            if symbol in interesting:
+                all_paths[symbol] = find_paths(x, y, maze)
+    return all_paths
 
 
-def find_reachable_keys(start, ignore_positions):
-    """Use a flood fill to find the distances to all reachable keys."""
-    queue = [start]
-    seen = set()
-    keys_found = []  # value = distance
-    n_steps = 0
-    while queue:
-        new_queue = []
-        n_steps += 1
-        for current_pos in queue:
-            for neighbor in maze[current_pos]:
-                if neighbor in seen:
+def part_1(maze):
+    all_paths = build_maze(maze)
+    keys = frozenset({k for k in all_paths if k in KEYS})
+    # info = (current location, collected keys): distance so far
+    info = {('@', frozenset()): 0}
+    for _ in keys:
+        new_info = {}
+        for item in info:
+            current_position = item[0]
+            collected_keys = item[1]
+            current_distance = info[item]
+            for next_key in keys:
+                if next_key in collected_keys:
                     continue
-                if neighbor not in ignore_positions:
-                    if neighbor in doors_by_position:
+                distance, path = all_paths[current_position][next_key]
+                # Is the next key reachable?
+                is_reachable = all(
+                    (c in collected_keys or c.lower() in collected_keys) for c in path
+                )
+                if not is_reachable:
+                    continue
+                new_distance = current_distance + distance
+                new_keys = frozenset({next_key}.union(collected_keys))
+                if ((next_key, new_keys) not in new_info
+                        or new_distance < new_info[(next_key, new_keys)]):
+                    new_info[(next_key, new_keys)] = new_distance
+        info = new_info
+    return min(info.values())
+
+
+def part_2(maze):
+    # Update the maze:
+    maze = [list(row) for row in maze]
+    for y, row in enumerate(maze):
+        for x, symbol in enumerate(row):
+            if symbol == '@':
+                for (dx, dy) in [(0, 0), (1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    maze[y + dy][x + dx] = '#'
+                maze[y - 1][x - 1] = '1'
+                maze[y - 1][x + 1] = '2'
+                maze[y + 1][x - 1] = '3'
+                maze[y + 1][x + 1] = '4'
+    maze = ["".join(row) for row in maze]
+
+    # Same algorithm as in part one but now with four simultaneous positions.
+    # I'm sure I could write a shared function for part 1 and part 2 that
+    # contains this loop but I'm done with this day's challenge.
+    all_paths = build_maze(maze)
+    keys = frozenset(k for k in all_paths if k in KEYS)
+    # info is almost the same as in part 1 but now has the positions of
+    # all four robots.
+    info = {(('1', '2', '3', '4'), frozenset()): 0}
+    for _ in keys:
+        new_info = {}
+        for item in info:
+            current_positions = item[0]
+            collected_keys = item[1]
+            current_distance = info[item]
+            for next_key in keys:
+                if next_key in collected_keys:
+                    continue
+                for robot in range(4):
+                    if next_key not in all_paths[current_positions[robot]]:
                         continue
-                    if neighbor in key_positions:
-                        keys_found.append({
-                            "position": neighbor,
-                            "key": key_positions[neighbor],
-                            "n_steps": n_steps
-                        })
-                new_queue.append(neighbor)
-                seen.add(neighbor)
-        queue = new_queue
-    return keys_found
-
-
-def walk_maze(start, ignore_positions):
-    reachable_keys = find_reachable_keys(start, ignore_positions)
-    # print(reachable_keys)
-    if not reachable_keys:
-        return 0
-    distances = []
-    for key in reachable_keys:
-        ignore_positions_new = ignore_positions.copy()
-        ignore_positions_new.add(key["position"])
-        door = key["key"].upper()
-        if door in doors_by_character:
-            ignore_positions_new.add(doors_by_character[door])
-        next_distance = key["n_steps"] + walk_maze(
-            key["position"],
-            ignore_positions_new
-        )
-        distances.append(next_distance)
-    return min(distances)
-
-
-def part_1(maze_input):
-    start_pos = build_maze(maze_input)
-    # pprint(find_reachable_keys(start_pos, set()))
-    return walk_maze(start_pos, set())
+                    distance, path = all_paths[current_positions[robot]][next_key]
+                    is_reachable = all(
+                        (c in collected_keys or c.lower() in collected_keys) for c in path
+                    )
+                    if not is_reachable:
+                        continue
+                    new_distance = current_distance + distance
+                    new_keys = frozenset({next_key}.union(collected_keys))
+                    new_positions = list(current_positions)
+                    new_positions[robot] = next_key
+                    new_positions = tuple(new_positions)
+                    if ((new_positions, new_keys) not in new_info
+                            or new_distance < new_info[(new_positions, new_keys)]):
+                        new_info[(new_positions, new_keys)] = new_distance
+        info = new_info
+    return min(info.values())
 
 
 test_input_1 = """#########
@@ -162,11 +175,10 @@ test_input_5 = """########################
 assert part_1(test_input_1) == 8
 assert part_1(test_input_2) == 86
 assert part_1(test_input_3) == 132
-# assert part_1(test_input_4) == 136
+assert part_1(test_input_4) == 136
 assert part_1(test_input_5) == 81
-
-# print(part_1(test_input_4))
 
 with open("day_18_input.txt") as file:
     challenge_input = file.read().splitlines()
-# print(part_1(challenge_input))
+print(part_1(challenge_input))  # 4406
+print(part_2(challenge_input))  # 1964
