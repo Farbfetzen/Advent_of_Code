f7a7a8ddcdd04fe91a6b249d39667c63f2284074
bastihz <bastihz.dev@posteo.de>
2020-12-10 19:23:51 +0100

    Compress the sample inputs

--- 2020/day_09.py
+++ 2020/day_09.py
@@ -26,28 +26,8 @@ def part_2(numbers, target):
                 break
 
 
-test_input = """35
-20
-15
-25
-47
-40
-62
-55
-65
-95
-102
-117
-150
-182
-127
-219
-299
-277
-309
-576
-"""
-test_input = [int(i) for i in test_input.splitlines()]
+test_input = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182,
+              127, 219, 299, 277, 309, 576]
 assert part_1(test_input, 5) == 127
 assert part_2(test_input, 127) == 62
 
--- 2020/day_10.py
+++ 2020/day_10.py
@@ -29,54 +29,11 @@ def part_2(adapters):
     return possibilities[0]
 
 
-test_input_1 = """16
-10
-15
-5
-1
-11
-7
-19
-6
-12
-4
-""".splitlines()
-test_input_1 = [int(x) for x in test_input_1]
+test_input_1 = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
 assert part_1(test_input_1) == 7 * 5
 assert part_2(test_input_1) == 8
-test_input_2 = """28
-33
-18
-42
-31
-14
-46
-20
-48
-47
-24
-23
-49
-45
-19
-38
-39
-11
-1
-32
-25
-35
-8
-17
-7
-9
-4
-2
-34
-10
-3
-""".splitlines()
-test_input_2 = [int(x) for x in test_input_2]
+test_input_2 = [28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38,
+                39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3]
 assert part_1(test_input_2) == 22 * 10
 assert part_2(test_input_2) == 19208
 
