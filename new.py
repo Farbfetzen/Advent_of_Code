693bf9f4c0466322f5f2cb73f0824e580b5d4dcd
bastihz <bastihz.dev@posteo.de>
2020-12-11 13:21:15 +0100

    Use current year and day as default argument values

--- new.py
+++ new.py
@@ -1,34 +1,40 @@
 """Creates a script with some boilerplate code and downloads the input
-for a given day. The arguments are 'year' and 'day', like this:
-python new.py 2020 7
+for a given day. The optional arguments are 'year' and 'day' with the
+current year and day as default values.
+Example: python new.py 2020 7
 """
 
 
-import os
 import argparse
+import datetime
 import json
+import os
 import requests
 
 
+today = datetime.date.today()
 parser = argparse.ArgumentParser()
-parser.add_argument("year", type=int)
-parser.add_argument("day", type=int)
+parser.add_argument("year", type=int, default=today.year, nargs="?")
+parser.add_argument("day", type=int, default=today.day, nargs="?")
 args = parser.parse_args()
-
-year = str(args.year)
+year = args.year
 day = args.day
+if not (2015 <= year <= today.year):
+    raise ValueError(f"Year {year} outside of sensible range.")
+if not (1 <= day <= 25):
+    raise ValueError(f"Day {day} outside of sensible range.")
+
+year = str(year)
 base_path = f"day_{day:02}"
 script_path = os.path.join(year, base_path + ".py")
 relative_input_path = base_path + "_input.txt"
 input_path = os.path.join(year, relative_input_path)
-challenge_url = f"https://adventofcode.com/{year}/day/{day}"
-input_url = challenge_url + "/input"
-
 if os.path.exists(script_path) or os.path.exists(input_path):
     raise OSError("One or both files already exist.")
-
 if not os.path.exists(year):
     os.mkdir(year)
+challenge_url = f"https://adventofcode.com/{year}/day/{day}"
+input_url = challenge_url + "/input"
 
 script_content = f'''# {challenge_url}\n\n
 import collections
@@ -39,7 +45,7 @@ def part_1(foo):
     return 0\n\n
 def part_2(foo):
     return 0\n\n
-test_input = """
+test_input = """\\
 """.splitlines()
 # assert part_1(test_input) ==
 # assert part_2(test_input) ==\n\n
