a0dd2001421953906e68267d2621474827ed6c66
bastihz <bastihz.dev@posteo.de>
2020-12-16 01:57:35 +0100

    Vastly improve the speed of 2020 day 15
    by using a list of single integers instead of a dict of deques
    and by changing the order of operations.

--- 2020/day_15.py
+++ 2020/day_15.py
@@ -1,23 +1,19 @@
 # https://adventofcode.com/2020/day/15
 
 
-from collections import defaultdict, deque
-
-
 def play(numbers, end_turn):
-    memory = defaultdict(lambda: deque([], maxlen=2))
-    for turn, n in enumerate(numbers, start=1):
-        memory[n].append(turn)
-    last_num = n
-    for turn in range(turn+1, end_turn+1):
+    memory = [0] * end_turn
+    for turn, last_num in enumerate(numbers, start=1):
         last_seen = memory[last_num]
-        if len(last_seen) == 1:
-            last_num = 0
+        memory[last_num] = turn
+    for turn in range(turn+1, end_turn+1):
+        if last_seen > 0:
+            last_num = turn - last_seen - 1
         else:
-            last_num = last_seen[1] - last_seen[0]
-        memory[last_num].append(turn)
-    print(f"Output for {numbers} on turn {turn} = {last_num}.",
-          f"The memory has length {len(memory)}.")
+            last_num = 0
+        last_seen = memory[last_num]
+        memory[last_num] = turn
+    print(f"Output for {numbers} on turn {turn} = {last_num}.")
     return last_num
 
 
@@ -29,16 +25,16 @@ assert play((2, 3, 1), 2020) == 78
 assert play((3, 2, 1), 2020) == 438
 assert play((3, 1, 2), 2020) == 1836
 
-assert play((0, 3, 6), 30000000) == 175594
-assert play((1, 3, 2), 30000000) == 2578
-assert play((2, 1, 3), 30000000) == 3544142
-assert play((1, 2, 3), 30000000) == 261214
-assert play((2, 3, 1), 30000000) == 6895259
-assert play((3, 2, 1), 30000000) == 18
-assert play((3, 1, 2), 30000000) == 362
+assert play((0, 3, 6), 30_000_000) == 175594
+assert play((1, 3, 2), 30_000_000) == 2578
+assert play((2, 1, 3), 30_000_000) == 3544142
+assert play((1, 2, 3), 30_000_000) == 261214
+assert play((2, 3, 1), 30_000_000) == 6895259
+assert play((3, 2, 1), 30_000_000) == 18
+assert play((3, 1, 2), 30_000_000) == 362
 
 
 with open("day_15_input.txt") as file:
-    challenge_input = [int(x) for x in file.read().strip().split(",")]
+    challenge_input = [int(x) for x in file.read().split(",")]
 print(play(challenge_input, 2020))  # 240
-print(play(challenge_input, 30000000))  # 505
+print(play(challenge_input, 30_000_000))  # 505
