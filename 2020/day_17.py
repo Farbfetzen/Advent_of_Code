ab6961253b78d414201ff9a9bca76f238c24fff3
bastihz <bastihz.dev@posteo.de>
2020-12-17 15:56:54 +0100

    Increase speed by only keeping track of active cells

--- 2020/day_17.py
+++ 2020/day_17.py
@@ -4,50 +4,43 @@
 import itertools
 
 
-def run_reactor(input_txt, n_dimensions):
-    directions = list(itertools.product((-1, 0, 1), repeat=4))
-    directions.remove((0, 0, 0, 0))
+DIRECTIONS = list(itertools.product((-1, 0, 1), repeat=4))
+DIRECTIONS.remove((0, 0, 0, 0))
 
+
+def check_neighbors(x, y, z, w, value, old_state, new_state, _3d,
+                    neighbors_to_check_around=None):
+    n_neighbors = 0
+    for dx, dy, dz, dw in DIRECTIONS:
+        if _3d and dw != 0:
+            continue
+        neighbor_position = (x + dx, y + dy, z + dz, w + dw)
+        n_neighbors += old_state.get(neighbor_position, 0)
+        if neighbors_to_check_around is not None:
+            neighbors_to_check_around.add(neighbor_position)
+    if value and n_neighbors in (2, 3) or not value and n_neighbors == 3:
+        new_state[(x, y, z, w)] = True
+
+
+def run_reactor(input_txt, _3d=True):
     initial_state = {}
     z = w = 0
     for y, row in enumerate(input_txt.splitlines()):
         for x, char in enumerate(row):
             if char == "#":
                 initial_state[(x, y, z, w)] = True
-    # Add the neighbors of the active cells:
-    for (x, y, z, w), value in initial_state.copy().items():
-        for dx, dy, dz, dw in directions:
-            if n_dimensions == 3 and dw != 0:
-                continue
-            neighbor_position = (x + dx, y + dy, z + dz, w + dw)
-            if neighbor_position not in initial_state:
-                initial_state[neighbor_position] = False
 
     reactor_state = initial_state.copy()
     for _ in range(6):
-        new_state = reactor_state.copy()
-        protected = set()
+        new_state = {}
+        neighbors_to_check_around = set()
         for (x, y, z, w), value in reactor_state.items():
-            n_neighbors = 0
-            for dx, dy, dz, dw in directions:
-                if n_dimensions == 3 and dw != 0:
-                    continue
-                n_neighbors += reactor_state.get((x+dx, y+dy, z+dz, w+dw), 0)
-            if n_neighbors == 0 and (x, y, z, w) not in protected:
-                # Remove cells with no neighbors to reduce the size of the dict.
-                del new_state[(x, y, z, w)]
-            elif value and n_neighbors not in (2, 3):
-                new_state[(x, y, z, w)] = False
-            elif not value and n_neighbors == 3:
-                new_state[(x, y, z, w)] = True
-                # Also add all neighbors around the newly active cell:
-                for dx, dy, dz, dw in directions:
-                    if n_dimensions == 3 and dw != 0:
-                        continue
-                    neighbor_position = (x+dx, y+dy, z+dz, w+dw)
-                    protected.add(neighbor_position)
-                    if neighbor_position not in new_state:
-                        new_state[neighbor_position] = False
+            check_neighbors(x, y, z, w, value, reactor_state, new_state, _3d,
+                            neighbors_to_check_around)
+        # Check the neighbors of the active cells:
+        for (x, y, z, w) in neighbors_to_check_around:
+            check_neighbors(x, y, z, w, reactor_state.get((x, y, z, w), 0),
+                            reactor_state, new_state, _3d)
         reactor_state = new_state
     return sum(reactor_state.values())
 
@@ -57,10 +50,10 @@ test_input = """\
 ..#
 ###
 """
-assert run_reactor(test_input, 3) == 112
-assert run_reactor(test_input, 4) == 848
+assert run_reactor(test_input) == 112
+assert run_reactor(test_input, False) == 848
 
 with open("day_17_input.txt") as file:
     challenge_input = file.read()
-print(run_reactor(challenge_input, 3))  # 232
-print(run_reactor(challenge_input, 4))  # 1620
+print(run_reactor(challenge_input))  # 232
+print(run_reactor(challenge_input, False))  # 1620
