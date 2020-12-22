a57ab79661d4b693dc0bb59009c0aa5c8b8b2d8d
bastihz <bastihz.dev@posteo.de>
2020-12-22 13:26:01 +0100

    Tidy up day 19

--- 2020/day_19.py
+++ 2020/day_19.py
@@ -19,6 +19,7 @@ def parse_input(input_txt):
 
 
 def resolve_rules(rules, cache, i=0):
+    # Recursive search with memoization.
     if i in cache:
         return cache[i]
     rule = rules[i]
@@ -26,6 +27,8 @@ def resolve_rules(rules, cache, i=0):
         cache[i] = rule
         return rule
     out = []
+    # I call "option" the part of a rule that is delimited by a "|".
+    # Most rules have two options.
     for option in rule:
         temp = [resolve_rules(rules, cache, next_i) for next_i in option]
         out.extend("".join(x) for x in itertools.product(*temp))
@@ -39,48 +42,20 @@ def part_1(rules, messages):
 
 
 def part_2(rules, messages):
+    # For an explanation see the bottom of this script.
+
     # 8: 42 | 42 8
     rules[8] = [[42], [42, 8]]
     # 11: 42 31 | 42 11 31
     rules[11] = [[42, 31], [42, 11, 31]]
 
-    # - Rule 8 returns these:
-    #   (42), (42, 42), (42, 42, 42), ...
-    #   And rule 11 returns these:
-    #   (42, 31), (42, 42, 31, 31), (42, 42, 42, 31, 31, 31), ...
-    #
-    # - The loopy part comes in the second half of the rules. That means
-    #   [42] or [42, 31] are already resolved when it is reached.
-    #
-    # - In the test rules and challenge rules rule 0 points directly
-    #   to 8 and 11 IN THIS ORDER. That means every valid rule must follow
-    #   one of these patterns:
-    #   1. left + left:   42, 42, 31
-    #   2. left + right:  42, x * 42, x * 31
-    #   3. right + left:  x * 42, 42, 31
-    #   4. right + right: x * 42, y * 42, y * 31
-    #   They have in common that the left part is all 42, the right part
-    #   is all 31 and the number of 31 is less than the number of 42.
-    #
-    # - Remember: Solve the challenge for the rules given, not any
-    #   general rules! Start by resolving only rules 31 and 42.
-    #   Then check the patterns.
-    #
-    # - Complication: 42 and 31 have many possibilities.
-    #   But: both are always 5 characters long. I can split each message
-    #   into 5-character words.
-
-    # share the cache both searches:
-    rule_cache = {}
-
-    r31 = set(resolve_rules(rules, rule_cache, 31))
-    # print(r31)
-    len_31 = {len(x) for x in r31}
-
-    r42 = set(resolve_rules(rules, rule_cache, 42))
-    # print(r42)
-    len_42 = {len(x) for x in r42}
-
+    rule_cache = {}  # share the cache for both searches
+    r_31 = set(resolve_rules(rules, rule_cache, 31))
+    r_42 = set(resolve_rules(rules, rule_cache, 42))
+    # print(r_31)
+    # print(r_42)
+    len_31 = {len(x) for x in r_31}
+    len_42 = {len(x) for x in r_42}
     len_both = len_31 | len_42
     assert len(len_both) == 1
     word_len = len_both.pop()
@@ -92,11 +67,11 @@ def part_2(rules, messages):
 
         n_31 = 0
         for word in reversed(words):
-            if word in r31:
+            if word in r_31:
                 n_31 += 1
             else:
                 break
-        if 0 < n_31 < n_words / 2 and all(word in r42 for word in words[:-n_31]):
+        if 0 < n_31 < n_words/2 and all(word in r_42 for word in words[:-n_31]):
             sum_valid += 1
 
     return sum_valid
@@ -176,3 +151,31 @@ with open("day_19_input.txt") as file:
     challenge_input = parse_input(file.read())
 print(part_1(*challenge_input))  # 248
 print(part_2(*challenge_input))  # 381
+
+
+# Explanation for part 2:
+#
+# - Rule 8 returns these:
+#   (42), (42, 42), (42, 42, 42), ...
+#   And rule 11 returns these:
+#   (42, 31), (42, 42, 31, 31), (42, 42, 42, 31, 31, 31), ...
+#
+# - The loopy part comes in the second half of the rules. That means
+#   [42] or [42, 31] are already resolved when it is reached.
+#
+# - In the test rules and challenge rules rule 0 points directly
+#   to 8 and 11 IN THIS ORDER. That means every valid rule must follow
+#   one of these patterns:
+#   1. left + left:   42, 42, 31
+#   2. left + right:  42, 42 * x, 31 * x
+#   3. right + left:  42 * x, 42, 31
+#   4. right + right: 42 * x, 42 * y, 31 * y
+#   They have in common that the left part is all 42, the right part
+#   is all 31 and the number of 31 is less than the number of 42.
+#
+# - Remember: Solve the challenge for the rules given, not any
+#   general rules! Start by resolving only rules 31 and 42.
+#   Then check the patterns.
+#
+# - Complication: 42 and 31 have many possibilities. But both are always
+#   the same length. I can split each message into n-character words.
