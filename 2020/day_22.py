23e4c494eef855d62fe35759337a5800c207ee50
BastiHz <bastihz.dev@posteo.de>
2020-12-26 17:30:10 +0100

    2020 day 22

--- 2020/day_22.py
+++ 2020/day_22.py
@@ -1,39 +1,78 @@
 # https://adventofcode.com/2020/day/22
 
 
-import collections
+from collections import deque
+from itertools import islice
+from copy import deepcopy
 
 
 def parse_input(input_txt):
     decks = []
     for deck in input_txt.split("\n\n"):
         deck = [int(card) for card in deck.splitlines()[1:]]
-        decks.append(collections.deque(deck))
+        decks.append(deque(deck))
+    decks = tuple(decks)
     return decks
 
 
+def calculate_score(cards):
+    return sum(i * v for i, v in enumerate(reversed(cards), start=1))
+
+
 def part_1(decks):
-    winner = None
-    while winner is None:
-        a = decks[0].popleft()
-        b = decks[1].popleft()
-        if a > b:
-            decks[0].extend((a, b))
+    decks = deepcopy(decks)
+    while True:
+        p0 = decks[0].popleft()
+        p1 = decks[1].popleft()
+        if p0 > p1:
+            decks[0].extend((p0, p1))
             if len(decks[1]) == 0:
-                winner = decks[0]
+                return calculate_score(decks[0])
         else:
-            decks[1].extend((b, a))
+            decks[1].extend((p1, p0))
             if len(decks[0]) == 0:
-                winner = decks[1]
-    winner.reverse()
-    score = 0
-    for i, v in enumerate(winner, start=1):
-        score += i * v
-    return score
+                return calculate_score(decks[1])
 
 
-def part_2(foo):
-    return 0
+def play_recursive(decks, top_level=False):
+    cache = set()
+    winner_game = None
+    while winner_game is None:
+        d = (tuple(decks[0]), tuple(decks[1]))
+        if d in cache:
+            return 0  # player 1 (= index 0) wins
+        else:
+            cache.add(d)
+        p0 = decks[0].popleft()
+        p1 = decks[1].popleft()
+        if p0 <= len(decks[0]) and p1 <= len(decks[1]):
+            new_deck = [
+                deque(islice(decks[0], 0, p0)),
+                deque(islice(decks[1], 0, p1))
+            ]
+            winner_round = play_recursive(new_deck)
+        elif p0 > p1:
+            winner_round = 0
+        else:
+            winner_round = 1
+        if winner_round == 0:
+            decks[0].extend((p0, p1))
+            if len(decks[1]) == 0:
+                winner_game = 0
+        else:
+            decks[1].extend((p1, p0))
+            if len(decks[0]) == 0:
+                winner_game = 1
+
+    if top_level:
+        return decks[winner_game]
+    else:
+        return winner_game
+
+
+def part_2(decks):
+    winner = play_recursive(deepcopy(decks), True)
+    return calculate_score(winner)
 
 
 test_input = """\
@@ -53,10 +92,9 @@ Player 2:
 """
 test_decks = parse_input(test_input)
 assert part_1(test_decks) == 306
-# assert part_2(test_input) ==
-
+assert part_2(test_decks) == 291
 
 with open("day_22_input.txt") as file:
     challenge_decks = parse_input(file.read())
 print(part_1(challenge_decks))  # 30138
-# print(part_2(challenge_input))  #
+print(part_2(challenge_decks))  # 31587
