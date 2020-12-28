659f8e190a7da7df8e71a668f45ef950d51a0ce3
bastihz <bastihz.dev@posteo.de>
2020-12-29 00:55:58 +0100

    Replace dicts with sets and let it run in any number of dimentions > 1

--- 2020/day_17.py
+++ 2020/day_17.py
@@ -1,62 +1,63 @@
 # https://adventofcode.com/2020/day/17
 
-
-import itertools
-
-
-DIRECTIONS = list(itertools.product((-1, 0, 1), repeat=4))
-DIRECTIONS.remove((0, 0, 0, 0))
+# I implemented it in a way such that the reactor should be able to run
+# in any number of dimensions > 1. I tested 2, 3, 4, and 5.
 
 
-def parse_input(input_txt):
-    initial_state = {}
-    z = w = 0
-    for y, row in enumerate(input_txt.splitlines()):
+from itertools import product
+from operator import add
+
+
+def check_neighbors(position, old_state, new_state, directions,
+                    candidates=None):
+    neighbor_positions = {tuple(map(add, position, direction))
+                          for direction in directions}
+    n_neighbors = len(neighbor_positions & old_state)
+    active = position in old_state
+    if active and n_neighbors in {2, 3} or not active and n_neighbors == 3:
+        new_state.add(position)
+    if candidates is not None:
+        candidates.update(neighbor_positions)
+
+
+def run_reactor(input_list, n_dim):
+    reactor_state = set()
+    extra_coordinates = [0] * (n_dim - 2)
+    for y, row in enumerate(input_list):
         for x, char in enumerate(row):
             if char == "#":
-                initial_state[(x, y, z, w)] = True
-    return initial_state
+                position = tuple([x, y] + extra_coordinates)
+                reactor_state.add(position)
 
+    directions = list(product((-1, 0, 1), repeat=n_dim))
+    directions.remove(tuple([0] * n_dim))
 
-def check_neighbors(x, y, z, w, value, old_state, new_state, _3d,
-                    neighbors_to_check_around=None):
-    n_neighbors = 0
-    for dx, dy, dz, dw in DIRECTIONS:
-        if _3d and dw != 0:
-            continue
-        neighbor_position = (x + dx, y + dy, z + dz, w + dw)
-        n_neighbors += old_state.get(neighbor_position, 0)
-        if neighbors_to_check_around is not None:
-            neighbors_to_check_around.add(neighbor_position)
-    if value and n_neighbors in (2, 3) or not value and n_neighbors == 3:
-        new_state[(x, y, z, w)] = True
-
-
-def run_reactor(initial_state, _3d=True):
-    reactor_state = initial_state.copy()
     for _ in range(6):
-        new_state = {}
-        neighbors_to_check_around = set()
-        for (x, y, z, w), value in reactor_state.items():
-            check_neighbors(x, y, z, w, value, reactor_state, new_state, _3d,
-                            neighbors_to_check_around)
+        new_state = set()
+        candidates = set()
+        for position in reactor_state:
+            check_neighbors(position, reactor_state, new_state, directions,
+                            candidates)
         # Check the neighbors of the active cells:
-        for (x, y, z, w) in neighbors_to_check_around:
-            check_neighbors(x, y, z, w, reactor_state.get((x, y, z, w), 0),
-                            reactor_state, new_state, _3d)
+        for position in candidates:
+            check_neighbors(position, reactor_state, new_state, directions)
         reactor_state = new_state
-    return sum(reactor_state.values())
+    return len(reactor_state)
 
 
 test_input = """\
 .#.
 ..#
 ###
-"""
-test_input = parse_input(test_input)
-assert run_reactor(test_input) == 112
-assert run_reactor(test_input, False) == 848
+""".splitlines()
+assert run_reactor(test_input, 3) == 112
+assert run_reactor(test_input, 4) == 848
 
-challenge_input = parse_input(open("day_17_input.txt").read())
-print(run_reactor(challenge_input))  # 232
-print(run_reactor(challenge_input, False))  # 1620
+with open("day_17_input.txt") as file:
+    challenge_input = file.read().splitlines()
+print(run_reactor(challenge_input, 3))  # 232
+print(run_reactor(challenge_input, 4))  # 1620
+
+# Not required but I was curious:
+print(run_reactor(challenge_input, 2))  # 30
+print(run_reactor(challenge_input, 5))  # 10632 (takes about a minute)
