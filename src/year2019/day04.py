0b8f1a6599b38f65af92dc682a6058b99373ec29
farbfetzen <bastihz.dev@posteo.de>
2022-12-21 11:31:12 +0100

    Convert 2019 day 4

--- src/year2019/day04.py
+++ src/year2019/day04.py
@@ -1,38 +1,37 @@
 # https://adventofcode.com/2019/day/4
 
+from src.util.types import Data, Solution
 
-def check_increasing_digits(password):
+
+def prepare_data(data: str) -> list[int]:
+    return [int(i) for i in data.split("-")]
+
+
+def check_increasing_digits(password: str) -> bool:
     return list(password) == sorted(password)
 
 
-def check_adjacent_pairs(password):
-    return any(password[i] == password[i + 1] for i in range(len(password) - 1))
+def check_adjacent_pairs(password: str) -> bool:
+    return any(x == y for x, y in zip(password, password[1:]))
 
 
-def check_adjacent_pairs_group(password):
+def check_adjacent_pairs_group(password: str) -> bool:
     return 2 in (password.count(digit) for digit in password)
 
 
-# part 1 test
-assert check_increasing_digits("111111") and check_adjacent_pairs("111111")
-assert not (check_increasing_digits("223450") and check_adjacent_pairs("223450"))
-assert not (check_increasing_digits("123789") and check_adjacent_pairs("123789"))
+def count_passwords(password_range: list[int]) -> tuple[int, int]:
+    total_part_1 = 0
+    total_part_2 = 0
+    for password_num in range(password_range[0], password_range[1] + 1):
+        password = str(password_num)
+        if check_increasing_digits(password):
+            total_part_1 += check_adjacent_pairs(password)
+            total_part_2 += check_adjacent_pairs_group(password)
+    return total_part_1, total_part_2
 
-# part 2 test
-assert check_increasing_digits("112233") and check_adjacent_pairs_group("112233")
-assert not (check_increasing_digits("123444") and check_adjacent_pairs_group("123444"))
-assert check_increasing_digits("111122") and check_adjacent_pairs_group("111122")
 
-with open("../../input/2019-04-input.txt") as file:
-    password_range = file.read()
-password_range = [int(i) for i in password_range.split("-")]
-total_part_1 = 0
-total_part_2 = 0
-for pw in range(password_range[0], password_range[1] + 1):
-    pw = str(pw)
-    if not check_increasing_digits(pw):
-        continue
-    total_part_1 += check_adjacent_pairs(pw)
-    total_part_2 += check_adjacent_pairs_group(pw)
-print(total_part_1)  # 1650
-print(total_part_2)  # 1129
+def solve(data: Data) -> Solution:
+    solution = Solution()
+    challenge_data = prepare_data(data.input)
+    solution.part_1, solution.part_2 = count_passwords(challenge_data)
+    return solution
