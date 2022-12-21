1ad1934ce6de4b2e8e8cb7e9f4dabc81526a2370
farbfetzen <bastihz.dev@posteo.de>
2022-12-21 10:49:59 +0100

    Convert 2019 day 2

--- src/year2019/day02.py
+++ src/year2019/day02.py
@@ -1,30 +1,38 @@
 # https://adventofcode.com/2019/day/2
 
-
 import itertools
 
-from intcode import IntcodeComputer
+from src.util.types import Data, Solution
+from src.year2019.intcode import IntcodeComputer
 
 
-with open("../../input/2019-02-input.txt") as file:
-    program = [int(i) for i in file.read().split(",")]
+def prepare_data(data: str) -> IntcodeComputer:
+    program = [int(i) for i in data.split(",")]
+    program[1] = 12
+    program[2] = 2
+    return IntcodeComputer(program)
 
 
-# part 1
-program[1] = 12
-program[2] = 2
-computer = IntcodeComputer(program)
-computer.run()
-print(computer.intcode[0])  # 3101844
-
-
-# part 2
-target = 19690720
-for noun, verb in itertools.product(range(100), repeat=2):
-    computer.original_intcode[1] = noun
-    computer.original_intcode[2] = verb
-    computer.reset()
+def part_1(computer: IntcodeComputer) -> int:
     computer.run()
-    if computer.intcode[0] == target:
-        break
-print(100 * noun + verb)  # 8478
+    return computer.intcode[0]
+
+
+def part_2(computer: IntcodeComputer) -> int:
+    target = 19690720
+    for noun, verb in itertools.product(range(100), repeat=2):
+        computer.original_intcode[1] = noun
+        computer.original_intcode[2] = verb
+        computer.reset()
+        computer.run()
+        if computer.intcode[0] == target:
+            return 100 * noun + verb
+    raise RuntimeError("Should have returned a result.")
+
+
+def solve(data: Data) -> Solution:
+    solution = Solution()
+    challenge_data = prepare_data(data.input)
+    solution.part_1 = part_1(challenge_data)
+    solution.part_2 = part_2(challenge_data)
+    return solution
