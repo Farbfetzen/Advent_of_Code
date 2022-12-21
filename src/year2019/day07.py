842c1917e6d7a23e8f121991de995843a8668c8c
farbfetzen <bastihz.dev@posteo.de>
2022-12-21 13:55:48 +0100

    Convert 2019 day 7

--- src/year2019/day07.py
+++ src/year2019/day07.py
@@ -1,16 +1,20 @@
 # https://adventofcode.com/2019/day/7
 
-
 import itertools
 
-from intcode import IntcodeComputer
+from src.util.types import Data, Solution
+from src.year2019.intcode import IntcodeComputer
 
 
-def build_amps(code, feedback_mode=False, n=5):
-    return [IntcodeComputer(code, True, feedback_mode) for _ in range(n)]
+def prepare_data(data: str) -> list[int]:
+    return [int(x) for x in data.split(",")]
 
 
-def test_phase_setting(amps, phases):
+def build_amps(code: list[int], feedback_mode: bool) -> list[IntcodeComputer]:
+    return [IntcodeComputer(code, True, feedback_mode) for _ in range(5)]
+
+
+def test_phase_setting(amps: list[IntcodeComputer], phases: tuple[int, ...]) -> int:
     assert len(amps) == len(phases)
     signal = 0
     for amp, phase in zip(amps, phases):
@@ -25,7 +29,7 @@ def test_phase_setting(amps, phases):
     return signal
 
 
-def iterate_phases(amps, phase_range):
+def iterate_phases(amps: list[IntcodeComputer], phase_range: range) -> int:
     max_signal = 0
     for phase_settings in itertools.permutations(phase_range):
         max_signal = max(
@@ -35,33 +39,17 @@ def iterate_phases(amps, phase_range):
     return max_signal
 
 
-with open("../../input/2019-07-input.txt") as file:
-    program = [int(i) for i in file.read().split(",")]
+def part_1(data: list[int]) -> int:
+    return iterate_phases(build_amps(data, False), range(5))
 
-# part 1:
-test_code_1 = build_amps((3, 15, 3, 16, 1002, 16, 10, 16,
-                          1, 16, 15, 15, 4, 15, 99, 0, 0))
-assert test_phase_setting(test_code_1, (4, 3, 2, 1, 0)) == 43210
-test_code_2 = build_amps((3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23,
-                          101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0))
-assert test_phase_setting(test_code_2, (0, 1, 2, 3, 4)) == 54321
-test_code_3 = build_amps((3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31,
-                          1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31,
-                          1, 32, 31, 31, 4, 31, 99, 0, 0, 0))
-assert test_phase_setting(test_code_3, (1, 0, 4, 3, 2)) == 65210
-amplifiers = build_amps(program)
-print(iterate_phases(amplifiers, range(5)))  # 21000
 
-# part 2:
-test_code_4 = build_amps((3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1,
-                          27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99,
-                          0, 0, 5), feedback_mode=True)
-assert test_phase_setting(test_code_4, (9, 8, 7, 6, 5)) == 139629729
-test_code_5 = build_amps((3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007,
-                          54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1,
-                          12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55,
-                          2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6,
-                          99, 0, 0, 0, 0, 10), feedback_mode=True)
-assert test_phase_setting(test_code_5, (9, 7, 8, 5, 6)) == 18216
-amplifiers = build_amps(program, feedback_mode=True)
-print(iterate_phases(amplifiers, range(5, 10)))  # 61379886
+def part_2(data: list[int]) -> int:
+    return iterate_phases(build_amps(data, True), range(5, 10))
+
+
+def solve(data: Data) -> Solution:
+    solution = Solution()
+    challenge_data = prepare_data(data.input)
+    solution.part_1 = part_1(challenge_data)
+    solution.part_2 = part_2(challenge_data)
+    return solution
