f019c4b19897adb4b7b3474105f93a40c503d4d7
Farbfetzen <farbfetzen@posteo.de>
2025-05-03 21:45:44 +0200

    Rewrite 2021 day 23 to make it faster and improve the code

--- Python/src/year_2021/day_23.py
+++ Python/src/year_2021/day_23.py
@@ -1,187 +1,176 @@
 # https://adventofcode.com/2021/day/23
 
-from collections.abc import Sequence
+import bisect
+import itertools
 
+from src.util.exceptions import ResultExpectedError
 from src.util.inputs import Inputs
 from src.util.solution import Solution
-from src.util.util import Point2
 
 
-MOVEMENT_ENERGY = {"A": 1, "B": 10, "C": 100, "D": 1000}
+# I don't use Vector2 for the positions here because that makes it much slower. This solution runs both parts
+# for the sample and puzzle input in about 7-8 seconds on my machine. This performance is similar
+# to other python solutions I saw on reddit. I spent a lot of time on this to reduce the search space
+# and don't see much potential for improvement. It's good enough for me.
+
+
+# Amphipod positions mapped to their names. Ones that reached their final position are renamed to "X".
+type AmphipodPositionMap = dict[tuple[int, int], str]
+
+# Positions mapped to their connected positions and the distances to them.
+type Connections = dict[tuple[int, int], list[tuple[tuple[int, int], int]]]
+
+# All positions that can be used for moves.
+type Spaces = list[tuple[int, int]]
 
 
 class Solution2021Day23(Solution):
+    hallway_y = 1
+    home_room_x = {3: "A", 5: "B", 7: "C", 9: "D"}
+    energy_requirements = {"A": 1, "B": 10, "C": 100, "D": 1000}
+    satisfied_amphipod = "X"
 
     def solve(self, inputs: Inputs) -> None:
-        # TODO: Make it faster.
-        #  Maybe it's so slow because bad paths are explored preferentially?
-        #  Is there a better distance metric so good paths are explored first?
-        #  Simply sorting the queue by decreasing energy makes it much slower.
-        #  And maybe use faster data structures?
-        prepared_input = inputs.samples[0].splitlines()
-        self.sample_results_1.append(self.solve_1(prepared_input))
-        self.sample_results_2.append(self.solve_2(prepared_input))
+        diagram = inputs.samples[0].splitlines()
+        self.sample_results_1.append(self.solve_1(diagram))
+        self.sample_results_2.append(self.solve_2(diagram))
 
-        prepared_input = inputs.input.splitlines()
-        self.result_1 = self.solve_1(prepared_input)
-        self.result_2 = self.solve_2(prepared_input)
+        diagram = inputs.input.splitlines()
+        self.result_1 = self.solve_1(diagram)
+        self.result_2 = self.solve_2(diagram)
 
-    def solve_1(self, data: list[str]) -> int:
-        destinations = {
-            "A": ((3, 2), (3, 3)),
-            "B": ((5, 2), (5, 3)),
-            "C": ((7, 2), (7, 3)),
-            "D": ((9, 2), (9, 3)),
-        }
-        neighbor_map, start_amphipod_positions = self.parse_map(data)
-        return self.find_min_energy(neighbor_map, start_amphipod_positions, destinations)
+    def solve_1(self, diagram: list[str]) -> int:
+        amphipods, spaces = self.parse_diagram(diagram)
+        connections = self.find_connections(spaces)
+        return self.find_min_energy(amphipods, connections)
 
-    def solve_2(self, data: list[str]) -> int:
-        destinations = {
-            "A": ((3, 2), (3, 3), (3, 4), (3, 5)),
-            "B": ((5, 2), (5, 3), (5, 4), (5, 5)),
-            "C": ((7, 2), (7, 3), (7, 4), (7, 5)),
-            "D": ((9, 2), (9, 3), (9, 4), (9, 5)),
-        }
-        neighbor_map, start_amphipod_positions = self.parse_map(data[:3] + ["  #D#C#B#A#", "  #D#B#A#C#"] + data[3:])
-        return self.find_min_energy(neighbor_map, start_amphipod_positions, destinations)
+    def solve_2(self, diagram: list[str]) -> int:
+        diagram = diagram[:3] + ["  #D#C#B#A#", "  #D#B#A#C#"] + diagram[3:]
+        amphipods, spaces = self.parse_diagram(diagram)
+        connections = self.find_connections(spaces)
+        return self.find_min_energy(amphipods, connections)
 
-    @staticmethod
-    def parse_map(data: list[str]) -> tuple[dict[Point2, list[Point2]], dict[Point2, str]]:
-        relevant_symbols = {".", "A", "B", "C", "D"}
-        neighbor_map: dict[Point2, list[Point2]] = {}
-        amphipod_positions = {}
-        offsets = ((1, 0), (0, 1), (-1, 0), (0, -1))
-        for y, line in enumerate(data):
+    def parse_diagram(self, diagram: list[str]) -> tuple[AmphipodPositionMap, Spaces]:
+        amphipods: AmphipodPositionMap = {}
+        spaces: Spaces = []
+        max_y = len(diagram) - 1
+        for y, line in enumerate(diagram):
             for x, char in enumerate(line):
-                if char in relevant_symbols:
-                    position = Point2(x, y)
-                    neighbor_map[position] = []
-                    if char.isalpha():
-                        amphipod_positions[position] = char
-                    for dy, dx in offsets:
-                        neighbor = Point2(x + dx, y + dy)
-                        if data[neighbor.y][neighbor.x] in relevant_symbols:
-                            neighbor_map[position].append(neighbor)
-        return neighbor_map, amphipod_positions
+                position = (x, y)
+                if char == "." and diagram[y + 1][x] == "#":
+                    spaces.append(position)
+                elif char.isalpha():
+                    if char == self.home_room_x[x] and all(char == diagram[yy][x] for yy in range(y + 1, max_y)):
+                        # Mark amphipods that have reached their goal with "X" because they won't move.
+                        amphipods[position] = self.satisfied_amphipod
+                    else:
+                        amphipods[position] = char
+                        spaces.append(position)
+        return amphipods, spaces
 
-    def find_min_energy(
-            self,
-            neighbor_map: dict[Point2, list[Point2]],
-            start_amphipod_positions: dict[Point2, str],
-            destinations: dict[str, Sequence[tuple[int, int]]]
-    ) -> int:
-        forbidden_positions = self.get_forbidden_positions(destinations)
-        non_stop_positions = {pos for pos in neighbor_map.keys() if len(neighbor_map[pos]) == 3}
-        queue = [(0, start_amphipod_positions)]
-        seen = {}
-        min_energy = 10 ** 6
+    def find_connections(self, spaces: Spaces) -> Connections:
+        """Returns the positions of all reachable spaces and the distances to a space."""
+        connections: Connections = {space: [] for space in spaces}
+        for space, other in itertools.combinations(spaces, 2):
+            if self.hallway_y == space[1] == other[1]:
+                # Skip because amphipods won't move between hallway positions.
+                continue
+            if space[1] == self.hallway_y or other[1] == self.hallway_y:
+                # Hallway spaces can only point to room spaces.
+                # Room spaces can point to hallway spaces.
+                distance = self.manhattan_distance(space, other)
+                connections[space].append((other, distance))
+                connections[other].append((space, distance))
+            else:
+                # Room spaces can point to spaces in other rooms.
+                # Get the distance to the hallway, then the manhattan distance to the target.
+                distance = space[1] - self.hallway_y
+                distance += self.manhattan_distance((space[0], self.hallway_y), other)
+                connections[space].append((other, distance))
+                connections[other].append((space, distance))
+        return connections
+
+    @staticmethod
+    def manhattan_distance(a: tuple[int, int], b: tuple[int, int]) -> int:
+        return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+    def find_min_energy(self, amphipods: AmphipodPositionMap, connections: Connections) -> int:
+        seen_states: set[tuple[tuple[tuple[int, int], str], ...]] = set()
+        energy = 0
+        queue: list[tuple[int, AmphipodPositionMap]] = [(energy, amphipods)]
         while queue:
-            current_energy, current_positions = queue.pop()
+            energy, amphipods = queue.pop()
 
-            for position, name in current_positions.items():
-                current_destinations = destinations[name]
+            if all(amphipod == self.satisfied_amphipod for amphipod in amphipods.values()):
+                # All amphipods have reached their goal positions.
+                return energy
 
-                # Check if amphipod is in destination room and all lower amphipods are
-                # of the correct type.
-                if position in current_destinations:
-                    i = current_destinations.index(position)
-                    for dest in current_destinations[i + 1:]:
-                        if current_positions[dest] != name:
-                            break
-                    else:
-                        continue
+            state = self.tuple_amphipods(amphipods)
+            if state in seen_states:
+                continue
+            seen_states.add(state)
 
-                reachable_positions = self.get_reachable_positions(
-                        name, MOVEMENT_ENERGY[name], position, neighbor_map,
-                        current_positions, non_stop_positions, forbidden_positions[name],
-                        current_destinations
-                )
-                for new_position, additional_energy in reachable_positions:
-                    # Move amphipod down into destination.
-                    new_amphipod_positions = current_positions.copy()
-                    del new_amphipod_positions[position]
-                    new_amphipod_positions[new_position] = name
-                    new_energy = current_energy + additional_energy
-                    if self.check_finished(new_amphipod_positions, destinations) and new_energy < min_energy:
-                        min_energy = new_energy
-                        continue
-                    new_state = (new_energy, new_amphipod_positions)
-                    positions_hashable = frozenset((k, v) for k, v in new_amphipod_positions.items())
-                    if positions_hashable in seen:
-                        old_energy = seen[positions_hashable]
-                        if old_energy > new_energy:
-                            seen[positions_hashable] = new_energy
-                            queue.append(new_state)
-                    else:
-                        seen[positions_hashable] = new_energy
-                        queue.append(new_state)
-            queue.sort(key=lambda state: state[0])
-
-        return min_energy
-
-    @staticmethod
-    def get_forbidden_positions(destinations: dict[str, Sequence[tuple[int, int]]]) -> dict[str, set[tuple[int, int]]]:
-        """The rooms of other amphipod types are forbidden."""
-        forbidden_positions = {}
-        for name in destinations:
-            positions = set()
-            for other_name, other_positions in destinations.items():
-                if other_name == name:
+            for position, amphipod in amphipods.items():
+                if amphipod == self.satisfied_amphipod:
                     continue
-                positions.update(other_positions)
-            forbidden_positions[name] = positions
-        return forbidden_positions
+                for new_position, distance in self.find_moves(position, amphipod, amphipods, connections):
+                    new_amphipods = amphipods.copy()
+                    del new_amphipods[position]
+                    if new_position[1] == self.hallway_y:
+                        new_amphipods[new_position] = amphipod
+                    else:
+                        # Amphipod moves into a room. This is only possible if that is its home.
+                        new_amphipods[new_position] = self.satisfied_amphipod
+                    new_energy = energy + distance * self.energy_requirements[amphipod]
+                    # Insert while keeping the queue sorted by descending energy.
+                    bisect.insort(queue, (new_energy, new_amphipods), key=lambda x: -x[0])
+        raise ResultExpectedError
 
     @staticmethod
-    def check_finished(positions, destinations) -> bool:
-        for position, name in positions.items():
-            if position not in destinations[name]:
+    def tuple_amphipods(amphipods: AmphipodPositionMap) -> tuple[tuple[tuple[int, int], str], ...]:
+        """Convert amphipods to a tuple so they can be inserted into a set."""
+        return tuple(sorted(amphipods.items()))
+
+    def find_moves(
+            self,
+            position: tuple[int, int],
+            amphipod: str,
+            amphipods: AmphipodPositionMap,
+            connections: Connections
+    ) -> list[tuple[tuple[int, int], int]]:
+        """Find all spaces that are reachable from the current position and return them with their distances."""
+        moves: list[tuple[tuple[int, int], int]] = []
+        for connection in connections[position]:
+            target = connection[0]
+            if target in amphipods:
+                continue
+            if target[1] == self.hallway_y:
+                if self.check_path_clear(position, target, amphipods):
+                    moves.append(connection)
+            elif self.home_room_x[target[0]] == amphipod:
+                # Target is at the amphipod home position.
+                below = (target[0], target[1] + 1)
+                # If below is not in connections then there is a wall and this is the lowest position in the room.
+                if ((below not in connections or amphipods.get(below) == self.satisfied_amphipod)
+                        and self.check_path_clear(position, target, amphipods)):
+                    # Return only this move because moving anywhere else wouldn't make sense.
+                    return [connection]
+        return moves
+
+    def check_path_clear(
+            self,
+            position: tuple[int, int],
+            target: tuple[int, int],
+            amphipods: AmphipodPositionMap
+    ) -> bool:
+        # Check if the space directly above is empty. Other spaces above can be ignored
+        # because there are never gaps in rooms.
+        if (position[0], position[1] - 1) in amphipods:
+            return False
+        # Check if the hallway is clear between the two positions.
+        position_x = position[0]
+        target_x = target[0]
+        for x, y in amphipods:
+            if y == self.hallway_y and (position_x < x < target_x or position_x > x > target_x):
                 return False
         return True
-
-    @staticmethod
-    def get_reachable_positions(
-            name: str,
-            movement_energy: int,
-            start_position: Point2,
-            neighbor_map: dict[Point2, list[Point2]],
-            amphipod_positions: dict[Point2, str],
-            non_stop_positions: set[Point2],
-            forbidden_positions: set[tuple[int, int]],
-            destinations: Sequence[tuple[int, int]]
-    ) -> list[tuple[Point2, int]]:
-        visited = {start_position}
-        candidates = []
-        queue = [(start_position, 0)]
-        while queue:
-            current = queue.pop()
-            new_energy = current[1] + movement_energy
-            for neighbor_position in neighbor_map[current[0]]:
-                if neighbor_position not in visited and neighbor_position not in amphipod_positions:
-                    visited.add(neighbor_position)
-                    new_position_and_energy = (neighbor_position, new_energy)
-                    queue.append(new_position_and_energy)
-                    candidates.append(new_position_and_energy)
-        candidates = [c for c in candidates
-                      if c[0] not in non_stop_positions and c[0] not in forbidden_positions]
-
-        # Moving to the destination room only makes sense of there are no amphipods
-        # of the wrong type and no empty spaces below the candidate position.
-        for candidate in reversed(candidates):
-            if candidate[0] in destinations:
-                i = destinations.index(candidate[0])
-                for dest in destinations[i + 1:]:
-                    other = amphipod_positions.get(dest)
-                    if other is None or other != name:
-                        candidates.remove(candidate)
-                        break
-                else:
-                    # This is the best possible candidate position. Moving elsewhere
-                    # wouldn't make sense.
-                    return [candidate]
-
-        if start_position not in forbidden_positions and start_position not in destinations:
-            # The amphipod is in the hallway and can only move to its destination room.
-            candidates = [c for c in candidates if c[0] in destinations]
-        return candidates
