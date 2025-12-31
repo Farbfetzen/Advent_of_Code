c390d8b56ff8592db43d346777f5e4ce9b136cde
Farbfetzen <farbfetzen@posteo.de>
2025-12-31 19:16:42 +0100

    Improve the speed of 2021 day 23
    by utilizing an A* search, taking the remaining energy into account.
    The sample and challenge inputs now run in about 3.5s on my machine. It's no longer the slowest test.

--- Python/src/year_2021/day_23.py
+++ Python/src/year_2021/day_23.py
@@ -10,9 +10,7 @@ from src.util.solution import Solution
 
 
 # I don't use Vector2 for the positions here because that makes it much slower. This solution runs both parts
-# for the sample and puzzle input in about 6 seconds on my machine. This performance is similar
-# to other python solutions I saw on reddit. I spent a lot of time on this to reduce the search space
-# and don't see much potential for improvement. It's good enough for me.
+# for the sample and puzzle input in less than 4 seconds on my machine.
 
 type Position = tuple[int, int]
 
@@ -23,7 +21,7 @@ type AmphipodPositionMap = dict[Position, str]
 type AmphipodPositionTuple = tuple[tuple[Position, str], ...]
 
 # Positions mapped to their connected positions and the distances to them.
-type ConnectionMap = dict[Position, list[tuple[Position, int]]]
+type ConnectionMap = dict[Position, dict[Position, int]]
 
 # All positions that can be used for moves.
 type Spaces = list[Position]
@@ -31,7 +29,7 @@ type Spaces = list[Position]
 
 class Solution2021Day23(Solution):
     hallway_y = 1
-    home_room_x = {3: "A", 5: "B", 7: "C", 9: "D"}
+    home_room_x = {"A": 3, "B": 5, "C": 7, "D": 9}
     energy_requirements = {"A": 1, "B": 10, "C": 100, "D": 1000}
     satisfied_amphipod = "X"
 
@@ -63,7 +61,7 @@ class Solution2021Day23(Solution):
                 if char == "." and diagram[y + 1][x] == "#":
                     spaces.append(position)
                 elif char.isalpha():
-                    if char == self.home_room_x[x] and all(char == diagram[yy][x] for yy in range(y + 1, max_y)):
+                    if x == self.home_room_x[char] and all(char == diagram[yy][x] for yy in range(y + 1, max_y)):
                         # Mark amphipods that have reached their goal with "X" because they won't move.
                         amphipods[position] = self.satisfied_amphipod
                     else:
@@ -73,7 +71,7 @@ class Solution2021Day23(Solution):
 
     def find_connections(self, spaces: Spaces, amphipods: AmphipodPositionMap) -> ConnectionMap:
         """Returns the positions of all reachable spaces and the distances to a space."""
-        connection_map: ConnectionMap = {space: [] for space in spaces}
+        connection_map: ConnectionMap = {space: {} for space in spaces}
         for space, other in itertools.combinations(spaces, 2):
             if space[0] == other[0] or self.hallway_y == space[1] == other[1]:
                 # Skip because amphipods that are not satisfied must first move into the hallway
@@ -84,18 +82,20 @@ class Solution2021Day23(Solution):
                 # Hallway spaces can only point to room spaces.
                 # Room spaces can point to hallway spaces.
                 distance = self.manhattan_distance(space, other)
-                connection_map[space].append((other, distance))
-                connection_map[other].append((space, distance))
+                connection_map[space][other] = distance
+                connection_map[other][space] = distance
             else:
                 # Room spaces can point to spaces in other rooms if those belong to the correct amphipod type.
                 # Get the distance to the hallway, then the manhattan distance to the target.
                 distance = space[1] - self.hallway_y
                 distance += self.manhattan_distance((space[0], self.hallway_y), other)
 
-                if amphipods[space] == self.home_room_x[other[0]]:
-                    connection_map[space].append((other, distance))
-                if amphipods[other] == self.home_room_x[space[0]]:
-                    connection_map[other].append((space, distance))
+                if other[0] == self.home_room_x[amphipods[space]]:
+                    # The other space is the home of the amphipod at the current space.
+                    connection_map[space][other] = distance
+                if space[0] == self.home_room_x[amphipods[other]]:
+                    # The current space is the home of the amphipod at the other space.
+                    connection_map[other][space] = distance
         return connection_map
 
     @staticmethod
@@ -104,9 +104,13 @@ class Solution2021Day23(Solution):
 
     def find_min_energy(self, amphipods: AmphipodPositionMap, connection_map: ConnectionMap) -> int:
         seen_states: dict[AmphipodPositionTuple, int] = {}
-        queue: list[tuple[int, AmphipodPositionMap]] = [(0, amphipods)]
+        # The queue is sorted by total energy, which is the sum of energy used so far plus potential remaining energy.
+        # energy so far: sum of energy used to reach the current state
+        # potential remaining energy: estimated energy to reach the target state
+        # queue entry: (total energy, energy used so far, amphipod positions)
+        queue: list[tuple[int, int, AmphipodPositionMap]] = [(0, 0, amphipods)]
         while queue:
-            energy, amphipods = queue.pop()
+            _, energy, amphipods = queue.pop()
 
             if all(amphipod == self.satisfied_amphipod for amphipod in amphipods.values()):
                 # All amphipods have reached their goal positions.
@@ -131,8 +135,9 @@ class Solution2021Day23(Solution):
                         continue
                     seen_states[state] = new_energy
 
-                    # Insert while keeping the queue sorted by descending energy.
-                    bisect.insort(queue, (new_energy, new_amphipods), key=lambda x: -x[0])
+                    # Insert while keeping the queue sorted by descending total energy.
+                    total_energy = new_energy + self.estimate_potential_energy(new_amphipods, connection_map)
+                    bisect.insort(queue, (total_energy, new_energy, new_amphipods), key=lambda x: -x[0])
 
         raise ResultExpectedError
 
@@ -148,7 +153,7 @@ class Solution2021Day23(Solution):
         position_x, position_y = position
         if (position_x, position_y - 1) in amphipods:
             return moves
-        for connection in connection_map[position]:
+        for connection in connection_map[position].items():
             target = connection[0]
             if target in amphipods:
                 continue
@@ -156,7 +161,7 @@ class Solution2021Day23(Solution):
             if target_y == self.hallway_y:
                 if self.check_path_clear(position_x, target_x, amphipods):
                     moves.append(connection)
-            elif self.home_room_x[target_x] == amphipod:
+            elif target_x == self.home_room_x[amphipod]:
                 # Target is at the amphipod home position.
                 below = (target_x, target_y + 1)
                 # If below is not in connections then there is a wall and this is the lowest position in the room.
@@ -172,3 +177,23 @@ class Solution2021Day23(Solution):
             if y == self.hallway_y and (position_x < x < target_x or position_x > x > target_x):
                 return False
         return True
+
+    def estimate_potential_energy(self, amphipods: AmphipodPositionMap, connection_map: ConnectionMap) -> int:
+        """Return a lower bound for the remaining energy necessary for all amphipods to reach their goals.
+        The goal in this case is the top space in the home room. This ignores blocked paths.
+        """
+        estimate = 0
+        for position, amphipod in amphipods.items():
+            if amphipod == self.satisfied_amphipod:
+                continue
+            amphipod_home_x = self.home_room_x[amphipod]
+            distance = 0
+            if position[0] == amphipod_home_x:
+                # The amphipod is in its home room but, it's not satisfied because there's a stranger below
+                # It needs to move out and come back in.
+                position_outside = (amphipod_home_x + 1, self.hallway_y)
+                distance += connection_map[position][position_outside]
+                position = position_outside
+            distance += connection_map[position][(amphipod_home_x, self.hallway_y + 1)]
+            estimate += distance * self.energy_requirements[amphipod]
+        return estimate
