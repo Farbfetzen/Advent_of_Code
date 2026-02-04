46dc0354300990a837f9bb6a886fca9566f0169f
Farbfetzen <farbfetzen@posteo.de>
2026-02-04 22:42:18 +0100

    Create project "jvm"
    - Generate Gradle configuration using IntelliJ
    - Adjust the Gradle configuration manually
    - Add .sdkmanrc
    - Write a readme

---
+++ jvm/README.md
@@ -0,0 +1,4 @@
+TODO: Finish writing this readme.
+
+Project for solutions written in Java and maybe Kotlin some day.
+run: `gradle run --args="2025 17"`
---
+++ jvm/src/main/java/farbfetzen/advent_of_code_jvm/Main.java
@@ -0,0 +1,12 @@
+package farbfetzen.advent_of_code_jvm;
+
+import java.util.Arrays;
+
+@SuppressWarnings("squid:S1118")
+public class Main {
+
+    static void main(final String[] args) {
+        IO.println("args:");
+        IO.println(Arrays.asList(args));
+    }
+}
